ac66740b43a1ce5116f251b469073e69d1b90a99
dchardin <dch84121@gmail.com>
2017-02-26 18:06:18 -0500

    general update

--- class_car.php
+++ class_car.php
@@ -2,11 +2,24 @@
 
 class Car {
     
+    /**Lets create some properties**/
+    
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 4;
+    
     /**Lets create a method called "MoveWheels" **/
     
     function MoveWheels(){
         
         echo "The wheels are moving";
+        
+    }
+    
+    function TwoDoors(){
+        
+        $this->doors = 2; 
         
     }
     
@@ -62,13 +75,50 @@ $bmw->MoveWheels();
 
 echo "</br>";
 
+echo "The number of wheels on the bmw are: ";
+echo "</br>";
+echo "$bmw->wheels";
+echo "</br>";
+
+echo "The number of doors on the bmw are: ";
+echo "</br>";
+echo "$bmw->doors";
+echo "</br>";
+
 /** the symbol -> looks kinda funny to me since I am used to using . in java **/
+
+
 
 echo "Testing the wheels of the Mercedes Benz...";
 echo "</br>";
 
 $mercedes_benz->MoveWheels();
 
+echo "</br>";
+
+/** override the class property wheels for the object mercedes_benz **/
+$mercedes_benz->wheels = 8;
+
+echo "The number of wheels on the mercedes benz are: ";
+echo "</br>";
+echo "$mercedes_benz->wheels";
+echo "</br>";
+
+echo "The number of doors on the mercedes benz are: ";
+echo "</br>";
+echo "$mercedes_benz->doors";
+echo "</br>";
+
+echo "Setting the car to coup configuration";
+$mercedes_benz->TwoDoors();
+echo "The number of doors on the mercedes benz are: ";
+echo "</br>";
+echo "$mercedes_benz->doors";
+echo "</br>";
+
+echo "The number of doors on the mercedes benz are: ";
+echo "</br>";
+echo "$mercedes_benz->doors";
 echo "</br>";
 
 ?>
