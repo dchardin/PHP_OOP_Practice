7a00529f4c0d2d74caf094d30c2dd85bdb9073b4
dchardin <dch84121@gmail.com>
2017-02-26 16:26:20 -0500

    general update

---
+++ class_car.php
@@ -0,0 +1,20 @@
+<?php
+
+class Car {
+    
+    
+}
+
+if(class_exists("Car")) {
+    
+    echo "The class exists!";
+    
+}   else {
+    
+    echo "The class does not exist.";
+    
+}
+
+
+
+?>
